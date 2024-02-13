fdec2d1b61f85dd8231449070b7543caec34f91d
Nathanael Huffman <nathanael@oxidecomputer.com>
2024-02-13 15:51:32 -0600

    Added basic arbiter

---
+++ hdl/ip/vhd/arb_mux_demux/basic_arbiter.vhd
@@ -0,0 +1,109 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at https://mozilla.org/MPL/2.0/.
+--
+-- Copyright 2024 Oxide Computer Company
+
+-- Note: Documentation can be rendered in VSCode using the TerosHDL 
+-- plugin: https://terostechnology.github.io/terosHDLdoc/
+
+
+--! A simple request arbiter that can do priority or round-robbin arbitration
+--! Built generically using unconstrained arrays.
+--! In Priority mode, the LSB (0) gets the highest priority.
+--! In round-robbin mode, the LSB gets the priority only when there was not a grant in the previous
+--! cycle and two requests occur simultaneously.
+--! A goal is to do this in a single cycle, without having to scan the request bits so that
+--! this scales well with increasing vectors.
+--! This arbiter uses the fact that <number> AND <two's complement of number> gives
+--! you a vector with the lowest (right-most) bit of <number> set
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity basic_arbiter is
+generic(
+  --! Defaults to round-robin, set to `true` to enable LSB
+  --! as highest priority requester
+  PRIORITY : boolean := false
+);
+port(
+    clk        : in    std_logic;
+    reset      : in    std_logic; 
+    --! Request vector blocks requesting arbitration must
+    --! assert their request until grant has been asserted
+    --! grant remains until request is de-asserted.
+    requests    : in    std_logic_vector;
+    --! This block asserts grant signals, and holds them
+    --! until the the associated request signal drops.
+    grants  : out   std_logic_vector
+);
+end entity;
+architecture rtl of basic_arbiter is
+  constant ZEROS         : std_logic_vector(requests'range) := (others => '0');
+signal  requests_int     : unsigned(requests'range);
+signal  requests_masked_twos    : unsigned(requests'range);
+signal  requests_masked  : unsigned(requests'range);
+signal  requests_last    : unsigned(requests'range);
+signal  grants_int       : unsigned(requests'range);
+signal  grants_last      : unsigned(requests'range);
+signal  grants_mask      : unsigned(requests'range);
+signal  req_vec_f_edge   : unsigned(requests'range);
+signal  req_f_edge       : std_logic;
+
+
+begin
+    requests_int  <= unsigned(requests);
+round_robbin_mode: if PRIORITY = false generate
+    -- This uses a grant-masking scheme to generate round-robin grant behavior.
+    -- We want to mask off the previous granted channel and any lower bits
+    -- so the upper bits get a round-robin chance.
+    grants_mask      <=  not (grants_last or (grants_last - 1) );
+                   
+    -- Mask the requests based on the current grant mask, unless we have no active requests or grants, in
+    -- which case, pass the unmasked vector through
+    requests_masked  <= requests_int and grants_mask when (requests_int and grants_mask) /= 0 else requests_int;
+else generate
+    -- In priority mode, we needn't mask grants since the priority encoder takes over
+    -- and we will allow multiple grants to the same requester to the exclusion of others
+    -- given the desired priority.
+    grants_mask       <= (others => '0');
+    requests_masked  <= requests_int;
+end generate;
+
+-- Build two's complement, remember basic boolean math? 2's complement = (not <dat>) + 1
+requests_masked_twos    <=  (not requests_masked) + 1;
+
+fedge_detects: for i in requests_int'range generate
+  req_vec_f_edge(i) <= '1' when requests_int(i) = '0' and requests_last(i) = '1' else '0';
+end generate;
+req_f_edge <= or req_vec_f_edge; --Unary reduction, active when any bit is active
+                    
+the_arbiter: process(clk)
+begin
+    if rising_edge(clk) then
+        -- Store history for edge detector
+        requests_last    <= requests_int;
+        -- To determine a grant, we take the currently active requests vector,
+        -- which is masked by the allowable requests in round-robbin mode, and then
+        -- bitwise AND that with it's two's complement
+        -- to get 1 bit active as our active grant.
+        grants_int     <= requests_masked and requests_masked_twos;
+        if req_f_edge = '1' then
+          -- Note: we expect requesters of this to hold request high until they both have the grant,
+          -- and are finished with their current transaction. This means that falling edges of
+          -- requests cause arbitration updates and we save the current grant for use in round-robin
+          -- arbitration next time.
+          grants_last    <= grants_int;
+        end if;
+        if reset = '1' then
+          grants       <= (others => '0');
+          grants_last      <= (others => '0');
+          requests_last    <= (others => '0');
+        end if;
+    end if;
+end process;
+    
+grants   <= std_logic_vector(grants);
+
+end architecture;
