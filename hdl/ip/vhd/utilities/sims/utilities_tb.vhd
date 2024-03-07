94166fbc1be2eb21a96c602767e6596a6643d79b
Nathanael Huffman <nathanael@oxidecomputer.com>
2024-03-07 16:24:09 -0600

    Add smoke test for utilities

---
+++ hdl/ip/vhd/utilities/sims/utilities_tb.vhd
@@ -0,0 +1,62 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at https://mozilla.org/MPL/2.0/.
+--
+-- Copyright 2024 Oxide Computer Company
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library vunit_lib;
+context vunit_lib.vunit_context;
+
+use work.calc_pkg.all;
+use work.time_pkg.all;
+use work.transforms_pkg.all;
+
+entity utilities_tb is
+    generic(runner_cfg : string);
+end entity;
+
+architecture tb of utilities_tb is
+begin
+
+    bench:process
+        variable calc_result : unsigned(31 downto 0);
+        variable tmp_flip : std_logic_vector(7 downto 0);
+    begin
+        -- Always the first thing in the process, set up things for the VUnit test runner
+        test_runner_setup(runner, runner_cfg);
+        while test_suite loop
+            if run("calc_pkg_log2ceil_test") then
+                -- log2ceil for 8 should require 3 bits for 8 positions (including 0)
+                check(log2ceil(8) = 3, "log2ceil not working");
+                --log2ceil for 1 should require 1 bit for 1 position
+                check(log2ceil(1) = 1, "log2ceil not working");
+            elsif run("calc_pkg_sel_tests") then
+               check(sel(true, "true", "false") = "true");
+               check(sel(false, "true", "false") = "false");
+            elsif run("time_pkg_tests") then
+                calc_result := calc_us(desired_us => 1, clk_period_ns => 10, return_size => 32);
+                check_equal(calc_result, 100, "Nope");
+                calc_result := calc_ms(desired_ms => 1, clk_period_ns => 10, return_size => 32);
+                check_equal(calc_result, 100_000, "Nope");
+                calc_result := calc_ns(desired_ns => 10, clk_period_ns => 10, return_size => 32);
+                check_equal(calc_result, 1, "Nope");
+            elsif run("transform_pkg_test") then
+                tmp_flip := reverse(X"0F");
+                check_equal(tmp_flip, std_logic_vector'(X"F0"), "Nope");
+            
+            end if;
+        end loop;
+           
+           
+        wait for 100 ns;
+        test_runner_cleanup(runner);
+        wait;
+        end process;
+        
+        -- Example total test timeout dog
+        test_runner_watchdog(runner, 10 ms);
+    end tb;
--- hdl/ip/vhd/utilities/time_pkg.vhd
+++ hdl/ip/vhd/utilities/time_pkg.vhd
@@ -17,44 +17,44 @@ use ieee.math_real.all;
 --! can be used for constant determined at compile/elab time.
 package time_pkg is
     --! Determine clocks required at `clk_period_ns` to cross `desired_ms` time (in miliseconds)
-    function calc_ms(desired_ms : positive; clk_period_ns : positive; returned_size : positive) return unsigned;
+    function calc_ms(desired_ms : positive; clk_period_ns : positive; return_size : positive) return unsigned;
     --! Determine clocks required at `clk_period_ns` to cross `desired_us` time (in microseconds)
-    function calc_us(desired_us : positive; clk_period_ns : positive; returned_size : positive) return unsigned;
+    function calc_us(desired_us : positive; clk_period_ns : positive; return_size : positive) return unsigned;
     --! Determine clocks required at `clk_period_ns` to cross `desired_ns` time (in nanoseconds)
-    function calc_ns(desired_ns : positive; clk_period_ns : positive; returned_size : positive) return unsigned;
+    function calc_ns(desired_ns : positive; clk_period_ns : positive; return_size : positive) return unsigned;
 
     -- Not intended to be part of the public api, used by above functions
-    function private_internal_calc(desired: positive; clk_per_ns: positive; ret_size: positive; sacle_factor: positive) return unsigned;
+    function private_internal_calc(desired: positive; clk_per_ns: positive; return_size: positive; sacle_factor: positive) return unsigned;
 end package;
 
 package body time_pkg is
     --------------------------------------------------------------------------------
     -- calc_Xx functions. Figure out how many clock periods it takes to represent
     -- the requested number in human-friendly units
-    function calc_us(num_us : integer; clk_period_ns : integer; ret_size : integer) return unsigned is
+    function calc_us(desired_us : positive; clk_period_ns : positive; return_size : positive) return unsigned is
         constant scale_factor: integer := 10**3; --(us to ns = 10^3)
     begin
-        return private_internal_calc(num_us, clk_period_ns, ret_size, scale_factor);
+        return private_internal_calc(desired_us, clk_period_ns, return_size, scale_factor);
     end function;
    
-    function calc_ms(num_ms : integer; clk_period_ns : integer; ret_size : integer) return unsigned is
+    function calc_ms(desired_ms : positive; clk_period_ns : positive; return_size : positive) return unsigned is
         constant scale_factor: integer := 10**6; --(ms to ns = 10^6)
     begin
-        return private_internal_calc(num_ms, clk_period_ns, ret_size, scale_factor);
+        return private_internal_calc(desired_ms, clk_period_ns, return_size, scale_factor);
     end function;
 
 
-    function calc_ns(num_ns : integer; clk_period_ns : integer; ret_size : integer) return unsigned is
+    function calc_ns(desired_ns : positive; clk_period_ns : positive; return_size : positive) return unsigned is
         constant scale_factor: integer := 1; --(ns to ns = 1)
     begin
-        return private_internal_calc(num_ns, clk_period_ns, ret_size, scale_factor);
+        return private_internal_calc(desired_ns, clk_period_ns, return_size, scale_factor);
     end function;
     
     -- Private helper function, not intended to be part of the public api, used by above functions
-    function private_internal_calc(desired: positive; clk_per_ns: positive; ret_size: positive; sacle_factor: positive) return unsigned is
-        variable ret_num : unsigned(ret_size - 1 downto 0);
+    function private_internal_calc(desired: positive; clk_per_ns: positive; return_size: positive; sacle_factor: positive) return unsigned is
+        variable ret_num : unsigned(return_size - 1 downto 0);
     begin
-         -- Not all values work out evenly since we're dividing, so we round up the value as needed
+        -- Not all values work out evenly since we're dividing, so we round up the value as needed
         -- to be greater than or equal to the requested value 
         ret_num := to_unsigned(natural(ceil(real(desired * sacle_factor) / real(clk_per_ns))), ret_num'length);
         return ret_num;
