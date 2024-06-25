fcb8c6cd6eef097ea2c9695cc35dd55320d198cf
Nathanael Huffman <nathanael@oxidecomputer.com>
2024-06-25 16:10:03 -0500

    Add pinout to the top and the constraints file

--- hdl/projects/grapefruit/grapefruit_top.vhd
+++ hdl/projects/grapefruit/grapefruit_top.vhd
@@ -1,3 +1,10 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at https://mozilla.org/MPL/2.0/.
+--
+-- Copyright 2024 Oxide Computer Company
+
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -8,16 +15,200 @@ entity grapefruit_top is
         reset_l : in    std_logic;
 
         ledn : out   std_logic
+        clk   : in    std_logic;
+        reset_l : in    std_logic; -- SP_TO_FPGA_LOGIC_RESET_L
+        seq_reg_to_sp_V3P3_pg: out std_logic;
+        seq_reg_to_sp_v1p2_pg: out std_logic;
+        sp5_to_sp_present_l : out std_logic;
+        sp_to_sp5_prochot_l : in std_logic;
+        sp5_to_sp_sp5R : out std_logic_vector(4 downto 1);
+        sp5_to_sp_coretype : out std_logic_vector(2 downto 0);
+        sp_to_ignit_fault_l: in std_logic;
+        sp5_to_sp_int_l : out std_logic;
+        sp_to_sp5_int_l : in std_logic;
+        sp_to_sp5_nmi_sync_flood_l: in std_logic;
+        nic_to_sp_gpio0_present1: out std_logic;
+        nic_to_sp_gpio0_present2: out std_logic;
+        rot_to_ignit_fault_l: in std_logic;
+
+        fmc_sp_to_fpga_clk : in std_logic;
+        fmc_sp_to_fpga_oe_l : in std_logic;
+        fmc_sp_to_fpga_we_l : in std_logic;
+        fmc_sp_to_fpga_wait_l : out std_logic;
+        fmc_sp_to_fpga_cs1_l: in std_logic;
+        fmc_sp_to_fpga_adv_l : in std_logic;
+        fmc_sp_to_fpga_bl0_l : in std_logic;
+        fmc_sp_to_fpga_bl1_l : in std_logic;
+        fmc_sp_to_fpga_da : inout std_logic_vector(15 downto 0);
+        fmc_sp_to_fpga_a : in std_logic_vector(19 downto 16);
+
+        seq_to_sp_misc_a : in std_logic;
+        seq_to_sp_misc_b : in std_logic;
+        seq_to_sp_misc_c : in std_logic;
+        seq_to_sp_misc_d : in std_logic;
+
+        seq_to_sp_int_l : in std_logic;
+        fpga_to_sp_irq1_l : out std_logic;
+        fpga_to_sp_irq2_l : out std_logic;
+        fpga_to_sp_irq3_l : out std_logic;
+        fpga_to_sp_irq4_l : out std_logic;
+
+        uart0_sp_to_fpga_dat : in std_logic;
+        uart0_fpga_to_sp_dat: out std_logic;
+        uart0_sp_to_fpga_rts_l : in std_logic;
+        uart0_fpga_to_sp_rts_l: out std_logic;
+
+        uart1_sp_to_fpga_dat : in std_logic;
+        uart1_fpga_to_sp_dat: out std_logic;
+        uart1_sp_to_fpga_rts_l : in std_logic;
+        uart1_fpga_to_sp_rts_l: out std_logic;
+
+        uart_local_sp_to_fpga_dat : in std_logic;
+        uart_local_fpga_to_sp_dat: out std_logic;
+        uart_local_sp_to_fpga_rts_l : in std_logic;
+        uart_local_fpga_to_sp_rts_l: out std_logic;
+
+        sp_to_seq_system_reset_l : in std_logic;
+        seq_rev_id : in std_logic_vector(2 downto 0);
+
+        spi_fpga_to_flash_cs_l : out std_logic;
+        spi_fpga_to_flash_clk : out std_logic;
+        spi_fpga_to_flash_dat : inout std_logic_vector(3 downto 0);
+
+        spi_fpga_to_flash2_cs_l : out std_logic;
+        spi_fpga_to_flash2_clk : out std_logic;
+        spi_fpga_to_flash2_dat : inout std_logic_vector(3 downto 0);
+        
+        i3c_sp5_to_fpga_oe_n_3v3: out std_logic;
+        i3c_fpga_to_dimm_oe_n_3v3: out std_logic;
+
+        uart0_sp5_to_fpga_data: in std_logic;
+        uart0_fpga_to_sp5_data: out std_logic;
+        uart0_sp5_to_fpga_rts_l : in std_logic;
+        uart0_fpga_to_sp5_rts_l: out std_logic;
+
+        uart1_scm_to_hpm_dat: out std_logic;
+        uart1_hpm_to_scm_dat: in std_logic;
+
+        fpga_spare_1v8: out std_logic_vector(7 downto 0);
+        fpga_spare_3v3: out std_logic_vector(7 downto 0);
+
+        espi_hpm_to_scm_reset_l : in std_logic;
+        espi_scm_to_hpm_alert_l : out std_logic;
+        espi_hpm_to_scm_cs_l: in std_logic;
+        espi_hpm_to_scm_clk: in std_logic;
+        espi_hpm_to_scm_dat: inout std_logic_vector(3 downto 0);
+
+        i3c_hpm_to_scm_dimm0_abcdef_scl: inout std_logic;
+        i3c_hpm_to_scm_dimm0_abcdef_sda: inout std_logic;
+        i3c_hpm_to_scm_dimm0_ghijkl_scl: inout std_logic;
+        i3c_hpm_to_scm_dimm0_ghijkl_sda: inout std_logic;
+
+        i3c_hpm_to_scm_dimm1_abcdef_scl: inout std_logic;
+        i3c_hpm_to_scm_dimm1_abcdef_sda: inout std_logic;
+        i3c_hpm_to_scm_dimm1_ghijkl_scl: inout std_logic;
+        i3c_hpm_to_scm_dimm1_ghijkl_sda: inout std_logic;
+
+        hdt_scm_to_hpm_tck: out std_logic;
+        hdt_scm_to_spm_tms: out std_logic;
+        hdt_scm_to_spm_dat: out std_logic;
+        hdt_hpm_to_scm_dat: in std_logic;
+        scm_to_hpm_fw_recovery: out std_logic;
+        hpm_to_scm_stby_rdy: in std_logic;
+        scm_to_hpm_stby_en: out std_logic;
+        scm_to_hpm_stby_rst_l: out std_logic;
+        scm_to_hpm_pwrbtn_l: out std_logic;
+        hpm_to_scm_pwrok: in std_logic;
+        scm_to_hpm_dbreq_l: out std_logic;
+        hpm_to_scm_pcie_rst_buf_l: out std_logic;
+        hpm_to_scm_spare: in std_logic_vector(1 downto 0);
+        scm_to_hpm_rot_cpu_rst_l: out std_logic;
+        hpm_to_scm_chassis_intr_l: in std_logic;
+        hpm_to_scm_irq_l: in std_logic;
+        scm_to_hpm_virtual_reseat: out std_logic;
+        uart_spare_scm_to_hpm_dat: out std_logic;
+        uart_spare_hpm_to_scm_dat: in std_logic;
+
+        qspi0_hpm_to_scm_clk : in std_logic;
+        qspi0_hpm_to_scm_cs0_l: in std_logic;
+        qspi0_hpm_to_scm_cs1_l: in std_logic;
+        qspi0_hpm_to_scm_dat: inout std_logic_vector(3 downto 0);
+        sgpip_scm_to_hpm_clk : out std_logic;
+
+        sgpio_scm_to_hpm_dat: out std_logic_vector(1 downto 0);
+        sgpio_hpm_to_scm_dat: in std_logic_vector(1 downto 0);
+        sgpio_scm_to_hpm_ld: out std_logic_vector(1 downto 0);
+        sgpio_scm_to_hpm_reset_l: in std_logic;
+        sgpio_hpm_to_scm_intr_l: out std_logic;
+
+        qspi1_scm_to_hpm_clk : out std_logic;
+        qspi1_scm_to_hpm_cs_l: in std_logic;
+        qspi1_scm_to_hpm_dat: inout std_logic_vector(3 downto 0);
+        spi_hpm_to_scm_tpm_cs_l : in std_logic;
+
+        i2c_sp_to_fpga_scl: inout std_logic;
+        i2c_sp_to_fpga_sda: inout std_logic;
+
+        i2c_scm_to_hpm_scl0: inout std_logic;
+        i2c_scm_to_hpm_sda0: inout std_logic;
+        i2c_scm_to_hpm_scl1: inout std_logic;
+        i2c_scm_to_hpm_sda1: inout std_logic;
+        i2c_scm_to_hpm_sda3: inout std_logic;
+        i2c_scm_to_hpm_scl3: inout std_logic;
+        i2c_scm_to_hpm_sda4: inout std_logic;
+        i2c_scm_to_hpm_scl4: inout std_logic;
+        i2c_scm_to_hpm_sda5: inout std_logic;
+        i2c_scm_to_hpm_scl5: inout std_logic;
+        i2c_scm_to_hpm_sda8: inout std_logic;
+        i2c_scm_to_hpm_scl8: inout std_logic;
+        i2c_scm_to_hpm_sda9: inout std_logic;
+        i2c_scm_to_hpm_scl9: inout std_logic;
+        i2c_scm_to_hpm_sda11: inout std_logic;
+        i2c_scm_to_hpm_scl11: inout std_logic;
+        i2c_scm_to_hpm_sda12: inout std_logic;
+        i2c_scm_to_hpm_scl12: inout std_logic;
+
+        fpga_to_fruid_scl: inout std_logic;
+        fpga_to_fruid_sda: inout std_logic
     );
 end entity;
 
 architecture rtl of grapefruit_top is
 
     signal counter : unsigned(31 downto 0);
+    signal pll_locked_async: std_logic;
+    signal clk_125m : std_logic;
+    signal reset_125m : std_logic;
+    signal clk_200m : std_logic;
+    signal reset_200m : std_logic;
 
 begin
 
     tst: process(clk, reset_l)
+    -- Generate the clock signals for the necessary domains
+    -- 200MHz is targetted for the eSPI interface so we can run fabric
+    -- at ~3x the 66MHz SPI clock rate
+    pll: entity work.gfruit_pll
+        port map ( 
+          clk_50m => clk,
+          clk_125m => clk_125m,
+          clk_200m => clk_200m,
+          reset => not reset_l,
+          locked => pll_locked_async
+          
+        );
+
+    -- Reset synchronizer into the clock domains
+    reset_sync_inst: entity work.reset_sync
+     port map(
+        pll_locked_async => pll_locked_async,
+        clk_125m => clk_125m,
+        reset_125m => reset_125m,
+        clk_200m => clk_200m,
+        reset_200m => reset_200m
+    );
+
+    tst : process (clk, reset_l)
     begin
         if reset_l = '0' then
             counter <= (others => '0');
@@ -26,6 +217,7 @@ begin
         end if;
     end process;
 
-    ledn <= not counter(26);
+    fpga_spare_1v8(0) <= not counter(26);
 
+    
 end rtl;
