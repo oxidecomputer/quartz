aaede626f6163079c0bf03031b555dcfec4deba6
Nathanael Huffman <nathanael@oxidecomputer.com>
2025-01-21 09:26:12 -0600

    Address PR feedback

--- hdl/projects/cosmo_seq/clk_rst_infra/cosmo_clks_and_resets.vhd
+++ hdl/projects/cosmo_seq/clk_rst_infra/cosmo_clks_and_resets.vhd
@@ -2,7 +2,7 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
 --
--- Copyright 2024 Oxide Computer Company
+-- Copyright 2025 Oxide Computer Company
 
 -- Instantiate the PLLs and build out reset synchronizers for the various clock domains
 
