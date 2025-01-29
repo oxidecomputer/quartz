5b15f373dbcd40a23d4b23f5708f21229207bee2
Nathanael Huffman <nathanael@oxidecomputer.com>
2025-01-28 23:20:57 -0600

    Add shift_in_at_0 and shift_in_at_high functions to common transforms

--- hdl/ip/vhd/common/utils/sims/utilities_tb.vhd
+++ hdl/ip/vhd/common/utils/sims/utilities_tb.vhd
@@ -2,7 +2,7 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
 --
--- Copyright 2024 Oxide Computer Company
+-- Copyright 2025 Oxide Computer Company
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -27,6 +27,11 @@ begin
     bench: process
         variable calc_result : unsigned(31 downto 0);
         variable tmp_flip    : std_logic_vector(7 downto 0);
+        variable tmp_slv_right_lsb     : std_logic_vector(7 downto 0);
+        variable tmp_slv_left_lsb : std_logic_vector(0 to 7);
+        variable tmp_unsigned_right_lsb : unsigned(7 downto 0);
+        variable tmp_unsigned_left_lsb : unsigned(0 to 7);
+
     begin
         -- Always the first thing in the process, set up things for the VUnit test runner
         test_runner_setup(runner, runner_cfg);
@@ -46,9 +51,44 @@ begin
                 check_equal(calc_result, 100_000, "Nope");
                 calc_result := calc_ns(desired_ns => 10, clk_period_ns => 10, return_size => 32);
                 check_equal(calc_result, 1, "Nope");
-            elsif run("transform_pkg_test") then
+            elsif run("transform_pkg_reverse_test") then
                 tmp_flip := reverse(X"0F");
                 check_equal(tmp_flip, std_logic_vector'(X"F0"), "Nope");
+            elsif run("transform_pkg_shift_in_at_0_test") then
+                tmp_slv_right_lsb := x"00";  -- going to shift in a '1' at bit0 to get x"01"
+                tmp_slv_left_lsb := x"00"; -- going to shift in a '1' at bit 0 to get x"80"
+                tmp_unsigned_right_lsb := x"00";
+                tmp_unsigned_left_lsb := x"00";
+
+                -- slv and unsigned, non-reversed ranges
+                check_equal(shift_in_at_0(tmp_slv_right_lsb, '1'), std_logic_vector'(x"01"), "Incorrect shift in at 0, slv");
+                check_equal(shift_in_at_0(tmp_slv_right_lsb, '1'), tmp_slv_right_lsb(tmp_slv_right_lsb'high  - 1 downto 0) & '1', "Incorrect shift in at 0, slv");
+                check_equal(shift_in_at_0(tmp_unsigned_right_lsb, '1'), unsigned'(x"01"), "Incorrect shift in at 0, slv");
+                check_equal(shift_in_at_0(tmp_unsigned_right_lsb, '1'), tmp_unsigned_right_lsb(tmp_unsigned_right_lsb'high  - 1 downto 0) & '1', "Incorrect shift in at 0, unsigned");
+
+                -- slv and unsigned, reversed ranges
+                check_equal(shift_in_at_0(tmp_slv_left_lsb, '1'), std_logic_vector'(x"80"), "Incorrect shift in at 0, slv");
+                check_equal(shift_in_at_0(tmp_slv_left_lsb, '1'), '1' & tmp_slv_left_lsb(tmp_slv_left_lsb'low to tmp_slv_left_lsb'high - 1), "Incorrect shift in at 0, slv");
+                check_equal(shift_in_at_0(tmp_unsigned_left_lsb, '1'), unsigned'(x"80"), "Incorrect shift in at 0, slv");
+                check_equal(shift_in_at_0(tmp_unsigned_left_lsb, '1'), '1' & tmp_unsigned_left_lsb(tmp_unsigned_left_lsb'low to tmp_unsigned_left_lsb'high - 1), "Incorrect shift in at high, unsigned");
+
+            elsif run("transform_pkg_shift_in_at_high_test") then
+                tmp_slv_right_lsb := x"00";  -- going to shift in a '1' at high-bit to get x"80"
+                tmp_slv_left_lsb := x"00"; -- going to shift in a '1' at high-bit to get x"01"
+                tmp_unsigned_right_lsb := x"00";
+                tmp_unsigned_left_lsb := x"00";
+
+                -- slv and unsigned, non-reversed
+                check_equal(shift_in_at_high(tmp_slv_right_lsb, '1'), std_logic_vector'(x"80"), "Incorrect shift in at high, slv");
+                check_equal(shift_in_at_high(tmp_slv_right_lsb, '1'), '1' & tmp_slv_right_lsb(tmp_slv_right_lsb'high  - 1 downto 0), "Incorrect shift in at high, slv");
+                check_equal(shift_in_at_high(tmp_unsigned_right_lsb, '1'), std_logic_vector'(x"80"), "Incorrect shift in at high, slv");
+                check_equal(shift_in_at_high(tmp_unsigned_right_lsb, '1'), '1' & tmp_unsigned_right_lsb(tmp_unsigned_right_lsb'high  - 1 downto 0), "Incorrect shift in at high, unsigned");
+
+                -- -- slv and unsigned, reversed
+                check_equal(shift_in_at_high(tmp_slv_left_lsb, '1'), tmp_slv_left_lsb(tmp_slv_left_lsb'low + 1 to tmp_slv_left_lsb'high) & '1', "Incorrect shift in at high, slv");
+                check_equal(shift_in_at_high(tmp_slv_left_lsb, '1'), unsigned'(x"01"), "Incorrect shift in at high, slv");
+                check_equal(shift_in_at_high(tmp_unsigned_left_lsb, '1'), tmp_unsigned_left_lsb(tmp_unsigned_left_lsb'low + 1 to tmp_unsigned_left_lsb'high) & '1', "Incorrect shift in at high, unsigned");
+                check_equal(shift_in_at_high(tmp_unsigned_left_lsb, '1'), unsigned'(x"01"), "Incorrect shift in at high, unsigned");
             end if;
         end loop;
 
--- hdl/ip/vhd/common/utils/transforms_pkg.vhd
+++ hdl/ip/vhd/common/utils/transforms_pkg.vhd
@@ -2,27 +2,55 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
 --
--- Copyright 2024 Oxide Computer Company
+-- Copyright 2025 Oxide Computer Company
 
--- Note: Documentation can be rendered in VSCode using the TerosHDL
--- plugin: https://terostechnology.github.io/terosHDLdoc/
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
---! Helper functions for doing various bit transformations.
+-- Helper functions for doing various bit transformations.
 package transforms_pkg is
 
-    --! Reverse a vector. Based on Jonathan Bromley's vhdl mailing list answer:
-    --! https://groups.google.com/g/comp.lang.vhdl/c/eBZQXrw2Ngk/m/4H7oL8hdHMcJ?pli=1
+    -- Reverse a vector. Based on Jonathan Bromley's vhdl mailing list answer:
+    -- https://groups.google.com/g/comp.lang.vhdl/c/eBZQXrw2Ngk/m/4H7oL8hdHMcJ?pli=1
     function reverse (
         a : in std_logic_vector
     ) return std_logic_vector;
 
-    --! Same as above, but overloaded for unsigned types
+    -- Same as above, but overloaded for unsigned types
     function reverse (
         a : in unsigned
+    ) return unsigned;
+
+    -- Provides a shift-in at 0 helper for std_logic_vectors.
+    -- Returns a vector of the same direction as arg but shifted up 1 bit
+    -- with bit shifted in at 0. This will work for both ascending and descending
+    -- vectors, always putting the new bit in at 0 and shifting left or right accordingly.
+    function shift_in_at_0 (
+        arg: in std_logic_vector;
+        bit : in std_logic
+    ) return std_logic_vector;
+
+    -- Provides a shift-in at high helper for std_logic_vectors.
+    -- Returns a vector of the same direction as arg but shifted down 1 bit
+    -- with bit shifted in at arg'high. This will work for both ascending and descending
+    -- vectors, always putting the new bit in at arg'high and shifting left or right accordingly.
+    function shift_in_at_high (
+        arg: in std_logic_vector;
+        bit : in std_logic
+    ) return std_logic_vector;
+
+    -- Same as above, but overloaded for unsigned types
+    function shift_in_at_0 (
+        arg : in unsigned;
+        bit : in std_logic
+    ) return unsigned;
+
+     -- Same as above, but overloaded for unsigned types
+    function shift_in_at_high (
+        arg : in unsigned;
+        bit : in std_logic
     ) return unsigned;
 
 end package;
@@ -43,12 +71,65 @@ package body transforms_pkg is
         return result;
     end reverse;
 
-    --! Overload for unsigned
+    -- Overload for unsigned
     function reverse (
         a : in unsigned
     ) return unsigned is
     begin
         return unsigned(reverse(std_logic_vector(a)));
     end reverse;
+
+
+    function shift_in_at_0 (
+        arg : in unsigned;
+        bit : in std_logic
+    ) return unsigned is
+        variable result : unsigned(arg'range);
+    begin
+        -- ARG could be ascending or descending, but we're going to
+        -- be putting the new bit in at 0 regardless. This means that
+        -- new bit 7 is always going to get bit 6 regardless of whether this
+        -- ends up being a right or left shift due to vector ordering
+        for i in result'high downto result'low + 1 loop
+            result(i) := arg(i - 1);
+        end loop;
+        result(0) := bit;
+        return result;
+    end function;
+
+    function shift_in_at_high (
+        arg : in unsigned;
+        bit : in std_logic
+    ) return unsigned is
+        variable result : unsigned(arg'range);
+    begin
+         -- ARG could be ascending or descending, but we're going to
+        -- be putting the new bit in at high regardless. This means that
+        -- new bit 0 is always going to get bit 1 regardless of whether this
+        -- ends up being a right or left shift due to vector ordering
+        for i in result'high - 1 downto result'low loop
+            result(i) := arg(i + 1);
+        end loop;
+        result(result'high) := bit;
+        return result;
+
+    end function;
+
+
+    function shift_in_at_0 (
+        arg : in std_logic_vector;
+        bit : in std_logic
+    ) return std_logic_vector is
+    begin
+        return std_logic_vector(shift_in_at_0(unsigned(arg), bit));
+    end function;
+
+    function shift_in_at_high (
+        arg : in std_logic_vector;
+        bit : in std_logic
+    ) return std_logic_vector is
+    begin
+        return std_logic_vector(shift_in_at_high(unsigned(arg), bit));
+    end function;
 
 end package body;
