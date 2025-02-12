fae74745fe86850eaf9aec74a984f62896625089
Aaron-Hartwig <aaron@oxide.computer>
2025-02-12 17:00:26 -0600

    first pass at RTL

--- hdl/projects/cosmo_seq/spd_proxy/spd_proxy_top.vhd
+++ hdl/projects/cosmo_seq/spd_proxy/spd_proxy_top.vhd
@@ -9,6 +9,7 @@ use ieee.std_logic_1164.all;
 
 use work.i2c_common_pkg.all;
 use work.stream8_pkg;
+use work.time_pkg.all;
 use work.tristate_if_pkg.all;
 
 entity spd_proxy_top is
@@ -40,12 +41,23 @@ architecture rtl of spd_proxy_top is
     signal cpu_busy             : boolean;
     signal cpu_has_mux          : boolean;
 
-    signal need_start : boolean;
-    signal start_simulated      : boolean;
+    signal ctrlr_idle           : std_logic;
+    signal ctrlr_scl_if         : tristate;
+    signal ctrlr_sda_if         : tristate;
+    signal ctrlr_has_int_mux    : boolean;
 
-    signal fpga_idle    : std_logic;
+
     signal fpga_scl_if  : tristate;
     signal fpga_sda_if  : tristate;
+
+    constant CNTR_SIZE  : integer := 8;
+    constant SIM_START_HD_TICKS : std_logic_vector(CNTR_SIZE - 1 downto 0) :=
+        calc_ns(get_i2c_settings(FAST_PLUS).sta_su_hd_ns, CLK_PER_NS, CNTR_SIZE);
+    signal need_start       : boolean;
+    signal scl_sim          : std_logic;
+    signal sda_sim          : std_logic;
+    signal sda_sim_fedge    : std_logic;
+    signal start_simulated  : std_logic;
 
     signal tx_st_if : stream8_pkg.data_channel;
     signal rx_st_if : stream8_pkg.data_channel;
@@ -89,8 +101,8 @@ begin
             end if;
 
             -- The FPGA still owns the bus and the START hold time as elapsed. This means before
-            -- the mux is swapped we need to simulate a START condition.
-            if fpga_idle = '0' and cpu_scl_fedge = '1' then
+            -- the mux is swapped we need to simulate a START condition .
+            if ctrlr_idle = '0' and cpu_scl_fedge = '1' then
                 need_start  <= true;
             elsif start_simulated then
                 need_start  <= false;
@@ -98,8 +110,42 @@ begin
         end if;
     end process;
 
-    -- Mux control
-    cpu_has_mux <= cpu_busy and fpga_idle = '1' and not need_start;
+    start_generator: process(clk, reset)
+        variable sda_sim_next   : std_logic;
+    begin
+        if reset then
+            scl_sim         <= '1';
+            sda_sim         <= '1';
+            sda_sim_next    := '1';
+        elsif rising_edge(clk) then
+            -- we never need to worry about simulating a falling edge on SCL since that will happen
+            -- automatically when we cut over to the CPU bus where SCL and SDA are both low already.
+            scl_sim <= '1';
+            if need_start and not ctrlr_has_int_mux and start_simulated = '0' then
+                -- create the artifical START condition
+                sda_sim_next := '0';
+            else
+                sda_sim_next := '1';
+            end if;
+
+            sda_sim_fedge   <= '1' when sda_sim = '1' and sda_sim_next = '0' else '0';
+            sda_sim         <= sda_sim_next;
+        end if;
+    end process;
+
+    countdown_inst: entity work.countdown
+        generic map (
+            SIZE    => CNTR_SIZE
+        )
+        port map (
+            clk     => clk,
+            reset   => reset,
+            count   => SIM_START_HD_TICKS,
+            load    => sda_sim_fedge,
+            decr    => not sda_sim,
+            clear   => '0',
+            done    => start_simulated
+        );
 
     -- FPGA I2C controller
     i2c_ctrl_txn_layer_inst: entity work.i2c_ctrl_txn_layer
@@ -110,8 +156,8 @@ begin
         port map(
             clk         => clk,
             reset       => reset,
-            scl_if      => fpga_scl_if,
-            sda_if      => fpga_sda_if,
+            scl_if      => ctrlr_scl_if,
+            sda_if      => ctrlr_sda_if,
             cmd         => (
                 op      => RANDOM_READ,
                 addr    => b"1010000", -- 0x50
@@ -120,19 +166,32 @@ begin
             ),
             cmd_valid   => '1',
             abort       => cpu_start_detected,
-            core_ready  => fpga_idle,
+            core_ready  => ctrlr_idle,
             tx_st_if    => tx_st_if,
             rx_st_if    => rx_st_if
         );
 
-    -- CPU and FPGA to DIMM bus mux
+    -- This mux controls if the I2C controller or the simulated START generator control the
+    -- FPGA internal I2C bux.
+    ctrlr_has_int_mux   <= not need_start or ctrlr_idle = '0';
+    fpga_scl_if.o       <= ctrlr_scl_if.o when ctrlr_has_int_mux else scl_sim;
+    fpga_scl_if.oe      <= ctrlr_scl_if.oe when ctrlr_has_int_mux else not scl_sim;
+    fpga_sda_if.o       <= ctrlr_sda_if.o when ctrlr_has_int_mux else sda_sim;
+    fpga_sda_if.oe      <= ctrlr_sda_if.oe when ctrlr_has_int_mux else not sda_sim;
+
+    -- Break the controller input from the bus when it does not have this mux
+    ctrlr_scl_if.i      <= fpga_scl_if.i when ctrlr_has_int_mux else '1';
+    ctrlr_sda_if.i      <= fpga_sda_if.i when ctrlr_has_int_mux else '1';
+
+    -- This mux controls if the CPU or the FPGA control the bus out to the DIMMs
+    cpu_has_mux     <= cpu_busy and ctrlr_idle = '1' and not need_start;
     dimm_scl_if.o   <= cpu_scl_if.i when cpu_has_mux else fpga_scl_if.o;
     dimm_scl_if.oe  <= not cpu_scl_if.i when cpu_has_mux else fpga_scl_if.oe;
     dimm_sda_if.o   <= cpu_sda_if.i when cpu_has_mux else fpga_sda_if.o;
     dimm_sda_if.oe  <= not cpu_sda_if.i when cpu_has_mux else fpga_sda_if.oe;
 
-    -- Break the controller input from the bus when it doesn't have the bus
-    fpga_scl_if.i   <= '1' when cpu_has_mux else dimm_scl_if.i;
-    fpga_sda_if.i   <= '1' when cpu_has_mux else dimm_sda_if.i;
+    -- Break the fpga input from the bus when it doesn't have the bus
+    fpga_scl_if.i  <= '1' when cpu_has_mux else dimm_scl_if.i;
+    fpga_sda_if.i  <= '1' when cpu_has_mux else dimm_sda_if.i;
 
 end architecture;
