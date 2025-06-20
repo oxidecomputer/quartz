4dc91dc15204393a1a59a40704e32a848429d35f
Nathanael Huffman <nathanael@oxidecomputer.com>
2025-06-20 12:46:48 -0500

    UART debug

--- hdl/projects/cosmo_seq/debug_module/debug_module_top.vhd
+++ hdl/projects/cosmo_seq/debug_module/debug_module_top.vhd
@@ -13,6 +13,7 @@ use ieee.numeric_std_unsigned.all;
 use work.axil8x32_pkg.all;
 
 use work.debug_regs_pkg.all;
+use work.sp5_uart_subsystem_pkg.all;
 
 entity debug_module_top is
     port (
@@ -21,7 +22,7 @@ entity debug_module_top is
 
         axi_if : view axil_target;
 
-        dbg_uart_control : out uart_control_type
+        uart_dbg_if : view uart_dbg_dbg_if
 
     );
 end entity;
@@ -30,7 +31,18 @@ architecture rtl of debug_module_top is
     signal rdata : std_logic_vector(31 downto 0);
     signal active_read : std_logic;
     signal active_write : std_logic;
+    signal dbg_uart_control : uart_control_type;
+    signal uart_pin_status: uart_pin_status_type;
 begin
+
+    -- Assign the output(s):
+    uart_dbg_if.sp5_console_uart_to_header <= dbg_uart_control.sp5_to_header;
+    uart_pin_status.ipcc_sp_cts_l <= uart_dbg_if.sp_uart1.uart_cts_pin_copy; -- (to SP pins, output from FPGA)
+    uart_pin_status.ipcc_sp_rts_l <= uart_dbg_if.sp_uart1.uart_rts_pin_copy; -- (from SP pins, input to FPGA)
+    uart_pin_status.console_sp_rts_l <= uart_dbg_if.sp_uart0.uart_rts_pin_copy; -- (from SP pins, input to FPGA)
+    uart_pin_status.console_sp_cts_l <= uart_dbg_if.sp_uart0.uart_cts_pin_copy; -- (to SP pins, output from FPGA);
+    uart_pin_status.console_sp5_rts_l <= uart_dbg_if.host_uart0.uart_rts_pin_copy; -- (from SP5 pins, input to FPGA)
+    uart_pin_status.console_sp5_cts_l <= uart_dbg_if.host_uart0.uart_cts_pin_copy; -- (to SP5 pins, output from FPGA)
 
      axil_target_txn_inst: entity work.axil_target_txn
      port map(
@@ -80,6 +92,13 @@ begin
             if active_read then
                 case to_integer(axi_if.read_address.addr) is
                     when UART_CONTROL_OFFSET => rdata <= pack(dbg_uart_control);
+                    when SP_AXI_TO_CONSOLE_UART_USEDWDS_OFFSET => rdata <= resize(uart_dbg_if.sp_uart0.axi_to_uart_fifo_usedwds, 32);
+                    when SP_CONSOLE_UART_TO_AXI_USEDWDS_OFFSET => rdata <= resize(uart_dbg_if.sp_uart0.uart_to_axi_fifo_usedwds, 32);
+                    when SP5_AXI_TO_CONSOLE_UART_USEDWDS_OFFSET => rdata <= resize(uart_dbg_if.host_uart0.axi_to_uart_fifo_usedwds, 32);
+                    when SP5_CONSOLE_UART_TO_AXI_USEDWDS_OFFSET => rdata <= resize(uart_dbg_if.host_uart0.uart_to_axi_fifo_usedwds, 32);
+                    when SP5_AXI_TO_IPCC_UART_USEDWDS_OFFSET => rdata <= resize(uart_dbg_if.sp_uart1.axi_to_uart_fifo_usedwds, 32);
+                    when SP5_IPCC_UART_TO_AXI_USEDWDS_OFFSET => rdata <= resize(uart_dbg_if.sp_uart1.uart_to_axi_fifo_usedwds, 32);
+                    --when CONSOLE_UART_PIN_STATUS_OFFSET : integer := 20;
                     when others => rdata <= (others => '0');
                 end case;
             end if;
---
+++ hdl/projects/cosmo_seq/sp5_uart_subsystem/sp5_uart_subsystem_pkg.vhd
@@ -0,0 +1,62 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at https://mozilla.org/MPL/2.0/.
+--
+-- Copyright 2025 Oxide Computer Company
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use work.calc_pkg.all;
+
+package sp5_uart_subsystem_pkg is
+
+    constant CONSOLE_FIFO_DEPTH : integer := 256; -- FIFO depth for UARTs
+    constant IPCC_FIFO_DEPTH : integer := 4096; -- FIFO depth for IPCC UART
+
+    -- To the debug interface
+    type console_uart_dbg_t is record
+        uart_to_axi_fifo_usedwds : std_logic_vector(log2ceil(CONSOLE_FIFO_DEPTH) downto 0);
+        axi_to_uart_fifo_usedwds : std_logic_vector(log2ceil(CONSOLE_FIFO_DEPTH) downto 0);
+        uart_rts_pin_copy : std_logic;
+        uart_cts_pin_copy : std_logic; 
+    end record;
+    view console_uart_ss of console_uart_dbg_t is
+        uart_to_axi_fifo_usedwds : out;
+        axi_to_uart_fifo_usedwds : out;
+        uart_rts_pin_copy : out;
+        uart_cts_pin_copy : out; 
+    end view;
+    alias console_uart_dbg is console_uart_ss'converse;
+
+    type ipcc_uart_dbg_t is record
+        uart_to_axi_fifo_usedwds : std_logic_vector(log2ceil(IPCC_FIFO_DEPTH) downto 0);
+        axi_to_uart_fifo_usedwds : std_logic_vector(log2ceil(IPCC_FIFO_DEPTH) downto 0);
+        uart_rts_pin_copy : std_logic;
+        uart_cts_pin_copy : std_logic; 
+    end record;
+    view ipcc_uart1_ss of ipcc_uart_dbg_t is
+        uart_to_axi_fifo_usedwds : out;
+        axi_to_uart_fifo_usedwds : out;
+        uart_rts_pin_copy : out;
+        uart_cts_pin_copy : out; 
+    end view;
+    alias ipcc_uart1_dbg is ipcc_uart1_ss'converse;
+
+    -- Now wrap all of that up into one big record
+    type uart_dbg_t is record
+        sp5_console_uart_to_header : std_logic;
+        sp_uart0 : console_uart_dbg_t;
+        host_uart0 : console_uart_dbg_t;
+        sp_uart1 : ipcc_uart_dbg_t;
+    end record;
+    view uart_dbg_ss_if of uart_dbg_t is
+        sp5_console_uart_to_header : out;
+        sp_uart0 : view console_uart_ss;
+        host_uart0 : view console_uart_ss;
+        sp_uart1 : view ipcc_uart1_ss;
+    end view;
+    alias uart_dbg_dbg_if is uart_dbg_ss_if'converse;
+
+end package;
