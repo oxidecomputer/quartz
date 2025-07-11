5b37b58d3846be04ce7c5c9849635ee0fc29a96c
Nathanael Huffman <nathanael@oxidecomputer.com>
2025-07-11 14:19:56 -0500

    wait until i2c stop to update mux

--- hdl/ip/vhd/i2c/muxes/PCA9545ish/sims/i2c_pca9545ish_tb.vhd
+++ hdl/ip/vhd/i2c/muxes/PCA9545ish/sims/i2c_pca9545ish_tb.vhd
@@ -48,6 +48,7 @@ begin
         variable ack_queue        : queue_t              := new_queue;
         variable expected_ack_queue        : queue_t     := new_queue;
         variable ack_status : boolean := false;
+        variable actor : actor_t := find("i2c_ctrl_vc");
 
     begin
         -- Always the first thing in the process, set up things for the VUnit test runner
@@ -63,13 +64,14 @@ begin
                 -- Enable ch0 of the mux via i2c command
                 push_byte(tx_queue, to_integer(std_logic_vector'(8x"01")));
                 i2c_write_txn (net, mux0_addr, tx_queue, ack_queue);
+                wait_until_idle(net, actor);
                 check_true(contains_all_acks(ack_queue), "Either no acks or some Nacks were found");
                 -- Verify mux ch0 is selected
                 check_equal(mux0_sel, chA_selected, "CHA Mux not selected after write");
 
                 -- Now read back the register
                 i2c_read_txn(net, mux0_addr, 1, rx_queue, ack_status);
-                wait for 1 us;
+                wait_until_idle(net, actor);
                 check_true(ack_status, "Target didn't ack during readback");
                 check_equal(pop_byte(rx_queue), 1, "CHA Readback didn't return expected value");
 
@@ -78,6 +80,7 @@ begin
                 -- Enable ch0 of the mux via i2c command
                 push_byte(tx_queue, to_integer(std_logic_vector'(8x"01")));
                 i2c_write_txn (net, mux1_addr, tx_queue, ack_queue);
+                wait_until_idle(net, actor);
                 check_true(contains_all_acks(ack_queue), "Either no acks or some Nacks were found");
                 -- Verify mux chA is selected on the alternate address
                 -- not on the default address
@@ -85,7 +88,7 @@ begin
                 check_equal(mux1_sel, chA_selected, "CHA on Mux1 was not selected after write");
                 -- Now read back the register
                 i2c_read_txn(net, mux1_addr, 1, rx_queue, ack_status);
-                wait for 1 us;
+                wait_until_idle(net, actor);
                 check_true(ack_status, "Target didn't ack during readback");
                 check_equal(pop_byte(rx_queue), 1, "CHA Readback didn't return expected value");
             elsif run("nack-wrong-target") then
@@ -93,12 +96,14 @@ begin
                 push_byte(tx_queue, to_integer(std_logic_vector'(8x"01")));
                 -- Write *something* to the wrong target address, expecting a NACK
                 i2c_write_txn(net, 7x"75", tx_queue, ack_queue);
+                wait_until_idle(net, actor);
                 check_true(target_addr_nack(ack_queue), "Target unexpectedly ACK'd write at wrong address");
                 check_equal(mux0_sel, not_selected, "Mux not still disconnected after NACK'd write");
 
                 push_byte(tx_queue, to_integer(std_logic_vector'(8x"01")));
                 -- Write *something* to the wrong target address, expecting a NACK
                 i2c_read_txn(net, 7x"75", 1, rx_queue, ack_status);
+                wait_until_idle(net, actor);
                 check_false(ack_status, "Target ACK'd read at wrong address");
 
             elsif run("multi-channel-attempt") then
@@ -106,11 +111,13 @@ begin
                 -- Enable ch0 of the mux via i2c command
                 push_byte(tx_queue, to_integer(std_logic_vector'(8x"01")));
                 i2c_write_txn (net, mux0_addr, tx_queue, ack_queue);
+                wait_until_idle(net, actor);
                 -- Verify mux ch0 is selected
                 check_equal(mux0_sel, chA_selected, "CHA Mux not selected after write");
                 -- Now errantly try to enable multiple channels
                 push_byte(tx_queue, to_integer(std_logic_vector'(8x"03")));
                 i2c_write_txn (net, mux0_addr, tx_queue, ack_queue);
+                wait_until_idle(net, actor);
                 -- We expect a NACK on the control register 2nd byte, and the mux should remain in its previous state
                 push_boolean(expected_ack_queue, true);  -- ACK on the target address byte
                 push_boolean(expected_ack_queue, false);  -- NACK on the control register 2nd byte
@@ -123,11 +130,13 @@ begin
                 -- Enable chA of the mux via i2c command
                 push_byte(tx_queue, to_integer(std_logic_vector'(8x"01")));
                 i2c_write_txn (net, mux0_addr, tx_queue, ack_queue);
+                wait_until_idle(net, actor);
                 -- Verify mux0 chA is selected
                 check_equal(mux0_sel, chA_selected, "CHA Mux0 not selected after write");
                 -- Now errantly try to enable another mux
                 push_byte(tx_queue, to_integer(std_logic_vector'(8x"01")));
                 i2c_write_txn (net, mux1_addr, tx_queue, ack_queue);
+                wait_until_idle(net, actor);
                 -- We expect a NACK on the control register 2nd byte, and the mux should remain in its previous state
                 push_boolean(expected_ack_queue, true);  -- ACK on the target address byte
                 push_boolean(expected_ack_queue, false);  -- NACK on the control register 2nd byte
@@ -142,44 +151,52 @@ begin
                 -- Enable ch0 of the mux via i2c command
                 push_byte(tx_queue, to_integer(std_logic_vector'(8x"01")));
                 i2c_write_txn (net, mux0_addr, tx_queue, ack_queue);
+                wait_until_idle(net, actor);
                 -- Verify mux ch0 is selected
                 check_equal(mux0_sel, chA_selected, "CHA Mux not selected after write");
 
                 -- Now read back the register
                 i2c_read_txn(net, mux0_addr, 1, rx_queue, ack_status);
+                wait_until_idle(net, actor);
                 check_true(ack_status, "Target didn't ack during readback");
                 check_equal(pop_byte(rx_queue), 1, "CHA Readback didn't return expected value");
 
                 -- Enable ch1 of the mux via i2c command
                 push_byte(tx_queue, to_integer(std_logic_vector'(8x"02")));
                 i2c_write_txn (net, mux0_addr, tx_queue, ack_queue);
+                wait_until_idle(net, actor);
                 -- Verify mux ch1 is selected
                 check_equal(mux0_sel, chB_selected, "CHB Mux not selected after write");
 
                 -- Now read back the register
                 i2c_read_txn(net, mux0_addr, 1, rx_queue, ack_status);
+                wait_until_idle(net, actor);
                 check_true(ack_status, "Target didn't ack during readback");
                 check_equal(pop_byte(rx_queue), 2, "CHB Readback didn't return expected value");
 
                 -- Enable ch2 of the mux via i2c command
                 push_byte(tx_queue, to_integer(std_logic_vector'(8x"04")));
                 i2c_write_txn (net, mux0_addr, tx_queue, ack_queue);
+                wait_until_idle(net, actor);
                 -- Verify mux ch1 is selected
                 check_equal(mux0_sel, chC_selected, "CHC Mux not selected after write");
 
                 -- Now read back the register
                 i2c_read_txn(net, mux0_addr, 1, rx_queue, ack_status);
+                wait_until_idle(net, actor);
                 check_true(ack_status, "Target didn't ack during readback");
                 check_equal(pop_byte(rx_queue), 4, "CHC Readback didn't return expected value");
 
                 -- Disable all of the mux via i2c command
                 push_byte(tx_queue, to_integer(std_logic_vector'(8x"00")));
                 i2c_write_txn (net, mux0_addr, tx_queue, ack_queue);
+                wait_until_idle(net, actor);
                 -- Verify mux ch1 is selected
                 check_equal(mux0_sel, not_selected, "Some mux still not selected after write");
 
                 -- Now read back the register
                 i2c_read_txn(net, mux0_addr, 1, rx_queue, ack_status);
+                wait_until_idle(net, actor);
                 check_true(ack_status, "Target didn't ack during readback");
                 check_equal(pop_byte(rx_queue), 0, "Readback didn't return expected value 0");
 
