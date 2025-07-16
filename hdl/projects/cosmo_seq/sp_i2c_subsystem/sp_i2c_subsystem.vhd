c79b87fbb817f7f5b6d2948f1309ede51299fbce
Nathanael Huffman <nathanael@oxidecomputer.com>
2025-07-16 14:28:39 -0500

    Move to consolidated oximux16

--- hdl/projects/cosmo_seq/sp_i2c_subsystem/sp_i2c_subsystem.vhd
+++ hdl/projects/cosmo_seq/sp_i2c_subsystem/sp_i2c_subsystem.vhd
@@ -11,7 +11,6 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 use work.axil8x32_pkg;
-use work.pca9545_pkg.all;
 
 entity sp_i2c_subsystem is
     port(
@@ -36,22 +35,17 @@ end sp_i2c_subsystem;
 
 architecture rtl of sp_i2c_subsystem is
 
-    type io_i2c_addr_t is array (natural range <>) of std_logic_vector(6 downto 0);
-    type mux_sel_t is array (natural range <>) of std_logic_vector(1 downto 0);
 
-    constant mux_i2c_addr:  io_i2c_addr_t(0 to 2) := (
-        b"1110_000",  -- Mux 1: 0x70 M.2 A/B
-        b"1110_001",  -- Mux 2: 0x71 APML / SEC
-        b"1110_010"   -- Mux 3: 0x72 Fan VPD / NIC Therm
-    );
-    signal sp_tgt_scl : std_logic_vector(mux_i2c_addr'range);
-    signal sp_tgt_scl_o : std_logic_vector(mux_i2c_addr'range);
-    signal sp_tgt_scl_oe : std_logic_vector(mux_i2c_addr'range);
-    signal sp_tgt_sda : std_logic_vector(mux_i2c_addr'range);
-    signal sp_tgt_sda_o : std_logic_vector(mux_i2c_addr'range);
-    signal sp_tgt_sda_oe : std_logic_vector(mux_i2c_addr'range);
-    signal mux_is_active : std_logic_vector(mux_i2c_addr'range);
-    signal mux_sel : mux_sel_t(mux_i2c_addr'range);
+    signal mux_sel1_int : std_logic_vector(1 downto 0);
+    -- On the schematic we have the following muxes:
+    --  These are virtualized to i2c address 0x70
+    --  Mux 1: CHA (bit0): M.2 A, CHB (bit1): M.2 B, CHC (bit2): N/C
+    --  Mux 2: CHA (bit3): APML, CHB (bit4): SEC, CHC (bit5): N/C
+    --  Mux 3: CHA (bit6): Fan VPD, CHB (bit7): NIC Therm, CHC (bit8): N/C
+    -- bit9..15:
+    constant mux_i2c_addr: std_logic_vector(6 downto 0) :=  b"1110_000";
+
+    signal mux_is_active : std_logic;
     signal mux_reset : std_logic;
     constant deselected : std_logic_vector(1 downto 0) := "11";
 
@@ -68,49 +62,30 @@ begin
      -------------------------------------
     -- SP I2C STUFF
     -------------------------------------
-    -- i2c breakout phy consolidator for sp i2c muxes
-    sp_i2c_phy_consolidator_inst: entity work.i2c_phy_consolidator
+    oximux16_top_inst: entity work.oximux16_top
      generic map(
-        TARGET_NUM => mux_i2c_addr'length
+        i2c_addr => mux_i2c_addr
     )
      port map(
         clk => clk,
         reset => reset,
+        mux_reset => mux_reset,
+        allowed_to_enable => '1',
+        mux_is_active => mux_is_active,
         scl => sp_scl,
         scl_o => sp_scl_o,
         scl_oe => sp_scl_oe,
         sda => sp_sda,
         sda_o => sp_sda_o,
         sda_oe => sp_sda_oe,
-        tgt_scl => sp_tgt_scl,
-        tgt_scl_o => sp_tgt_scl_o,
-        tgt_scl_oe => sp_tgt_scl_oe,
-        tgt_sda => sp_tgt_sda,
-        tgt_sda_o => sp_tgt_sda_o,
-        tgt_sda_oe => sp_tgt_sda_oe
+        -- this is a terminology mismatch, because the schematic used
+        -- 1 indexing and we use zero indexing in the IP.
+        mux0_sel => mux_sel1_int, -- see power workaround below
+        mux1_sel => i2c_mux2_sel,
+        mux2_sel => i2c_mux3_sel,
+        mux3_sel => open, -- un-used on this i2c bus
+        mux4_sel => open -- un-used on this i2c bus
     );
-
-    -- 3 muxes here
-    mux_gen: for i in mux_i2c_addr'range generate
-        pca9545ish_top_inst: entity work.pca9545ish_top
-         generic map(
-            i2c_addr => mux_i2c_addr(i)
-        )
-         port map(
-            clk => clk,
-            reset => reset,
-            mux_reset => mux_reset,
-            allowed_to_enable => allowed_to_enable(mux_is_active, i),
-            mux_is_active => mux_is_active(i),
-            scl => sp_tgt_scl(i),
-            scl_o => sp_tgt_scl_o(i),
-            scl_oe => sp_tgt_scl_oe(i),
-            sda => sp_tgt_sda(i),
-            sda_o => sp_tgt_sda_o(i),
-            sda_oe => sp_tgt_sda_oe(i),
-            mux_sel => mux_sel(i)
-        );
-    end generate;
     
     -- Set up the mux-sels here
     -- Note that i2c_mux_1_sel was not properly isolated into the hotplug domain
@@ -128,13 +103,10 @@ begin
             i2c_mux1_sel <= deselected;
         elsif rising_edge(clk) then
             if in_a0 then
-                i2c_mux1_sel <= mux_sel(0);
+                i2c_mux1_sel <= mux_sel1_int;
             else
                 i2c_mux1_sel <= deselected;
             end if;
         end if;
     end process;
-    -- No workarounds needed for these:
-    i2c_mux2_sel <= mux_sel(1);
-    i2c_mux3_sel <= mux_sel(2);
 end rtl;
