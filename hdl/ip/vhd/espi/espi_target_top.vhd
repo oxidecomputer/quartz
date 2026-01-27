64f3b921815f502ad0ec92d8ff60801b102c8f76
Nathanael Huffman <nathanaelhuffman@users.noreply.github.com>
2026-01-27 14:12:32 -0500

    clear down espi FIFOs in a smarter way to prevent stale IPCC data from being present on next boot (#456)
    This adds some more sideband info to look at fifo states, improves the
    fifo resets and blocking of IPCC traffic in A2, will not accept IPCC
    traffic until the OOB channel is enabled, and adds a FIFO stuffer to
    better debug host drivers.

--- hdl/ip/vhd/espi/espi_target_top.vhd
+++ hdl/ip/vhd/espi/espi_target_top.vhd
@@ -82,7 +82,6 @@ architecture rtl of espi_target_top is
     signal sclk_syncd : std_logic;
     signal vwire_if : vwire_if_type;
     signal vwire_avail : std_logic;
-    signal msg_en : std_logic;
     signal qspi_cmd : byte_stream;
     signal qspi_resp : byte_stream;
     signal gen_cmd : byte_stream;
@@ -99,10 +98,15 @@ architecture rtl of espi_target_top is
     signal wait_states_slow: std_logic_vector(3 downto 0);
     signal post_code : std_logic_vector(31 downto 0);
     signal post_code_valid : std_logic;
-    signal espi_reset_strobe : std_logic;
+    signal espi_reset_strobe_125m : std_logic;
     signal espi_reset_strobe_syncd : std_logic;
     signal espi_link_layer : std_logic;
     signal dbg_espi_reset : std_logic;
+    signal to_host_tx_fifo_usedwds : std_logic_vector(12 downto 0);
+    signal ipcc_to_host_byte_cntr : std_logic_vector(31 downto 0);
+    signal oob_enabled : std_logic;
+    signal stuff_fifo :  std_logic;
+    signal stuff_wds : std_logic_vector(15 downto 0);
 
 begin
 
@@ -193,8 +197,6 @@ begin
         dbg_espi_reset => dbg_espi_reset
     );
 
-    -- combine our "real" espi reset with the debug one
-    espi_reset_strobe <= espi_link_layer or dbg_espi_reset;
 
     alert_needed_slow <= '1' when alert_needed else '0';
     alert_sync: entity work.meta_sync
@@ -211,11 +213,15 @@ begin
     port map(
        clk_launch => clk_200m,
        reset_launch => reset_200m,
-       pulse_in_launch => espi_reset_strobe,
+       pulse_in_launch => espi_link_layer,
        clk_latch => clk,
        reset_latch => reset,
-       pulse_out_latch => espi_reset_strobe_syncd
+       pulse_out_latch => espi_reset_strobe_125m
    );
+
+   -- combine our "real" espi reset with the debug one
+   -- we do this *after* we've crossed clock domains so this is all in the 125Mhz domain.
+    espi_reset_strobe_syncd <= espi_reset_strobe_125m or dbg_espi_reset;
 
 
    link_to_txn_bridge_inst: entity work.link_to_txn_bridge
@@ -224,7 +230,7 @@ begin
        reset_200m => reset_200m,
        clk => clk,
        reset => reset,
-       espi_reset_fast => espi_reset_strobe,
+       espi_reset_fast => espi_link_layer,
        espi_reset_slow => espi_reset_strobe_syncd,
        txn_gen_enabled => dbg_chan.enabled,
        qspi_cmd => qspi_cmd,
@@ -245,11 +251,14 @@ begin
        clk => clk,
        reset => reset,
        axi_if => axi_if,
-       msg_en => msg_en,
+       stuff_fifo => stuff_fifo,
+       stuff_wds => stuff_wds,
        dbg_chan => dbg_chan,
        post_code => post_code,
        post_code_valid => post_code_valid,
-       espi_reset => espi_reset_strobe_syncd
+       espi_reset => espi_reset_strobe_syncd,
+       to_host_tx_fifo_usedwds => to_host_tx_fifo_usedwds,
+       ipcc_to_host_byte_cntr => ipcc_to_host_byte_cntr
    );
 
     -- txn layer blocks
@@ -295,7 +304,8 @@ begin
             regs_if        => regs_if,
             qspi_mode      => qspi_mode,
             wait_states    => wait_states_slow,
-            flash_channel_enable => flash_channel_enable
+            flash_channel_enable => flash_channel_enable,
+            oob_enabled    => oob_enabled
         );
 
     -- flash access channel logic
@@ -322,6 +332,9 @@ begin
        clk => clk,
        reset => reset,
        espi_reset => espi_reset_strobe_syncd,
+       enabled => oob_enabled,
+       stuff_fifo => stuff_fifo,
+       stuff_wds => stuff_wds,
        host_to_sp_espi => host_to_sp_espi,
        sp_to_host_espi => sp_to_host_espi,
        to_sp_uart_data => to_sp_uart_data,
@@ -330,13 +343,14 @@ begin
        from_sp_uart_data => from_sp_uart_data,
        from_sp_uart_valid => from_sp_uart_valid,
        from_sp_uart_ready => from_sp_uart_ready,
-       msg_not_oob => msg_en,
        pc_free => pc_free,
        pc_avail => pc_avail,
        np_free => np_free,
        np_avail => np_avail,
        oob_avail => oob_avail,
-       oob_free => oob_free
+       oob_free => oob_free,
+       to_host_tx_fifo_usedwds => to_host_tx_fifo_usedwds,
+       ipcc_to_host_byte_cntr => ipcc_to_host_byte_cntr
    );
 
    -- vwire channel logic
