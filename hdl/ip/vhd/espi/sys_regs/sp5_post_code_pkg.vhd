e7694e95935ff48f7fc7760625061585ae1dafae
Nathanael Huffman <nathanaelhuffman@users.noreply.github.com>
2026-04-17 12:53:12 -0400

    Update hdl/ip/vhd/espi/sys_regs/sp5_post_code_pkg.vhd
    Co-authored-by: Aaron Hartwig <aaron@oxidecomputer.com>

--- hdl/ip/vhd/espi/sys_regs/sp5_post_code_pkg.vhd
+++ hdl/ip/vhd/espi/sys_regs/sp5_post_code_pkg.vhd
@@ -2,7 +2,7 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
 --
--- Copyright 2025 Oxide Computer Company
+-- Copyright 2026 Oxide Computer Company
 
 -- Composite record and views for exposing eSPI spec register
 -- values as a read-only interface in the sys_regs address space.
